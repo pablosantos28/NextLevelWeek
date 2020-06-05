b01d11671f12851c99833fbb70b776b2dc583094
pablosantos28 <65353785+pablosantos28@users.noreply.github.com>
2020-06-05 18:32:41 -0300

    Aula 04 e Criação de servidor
    Criação de servidor com técnicas de estrutura melhores com Nunjucks Templates.

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+// configurar pasta publica
+server.use(express.static("public"))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", function(req, res) {
+    return res.render("index.html", { title: "Um título"})
+})
+
+server.get("/create-point", function(req, res) {
+    return res.render("create-point.html")
+})
+
+server.get("/search", function(req, res) {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+//listen é ouvir
+server.listen(3000)
